25bef1f268ddb9df5d9a01c247d3260cc0766316
Jonathan Ward <jonathan.stuart.ward@gmail.com>
2015-05-13 14:48:36 +0100

    Removed aggregateD out to a separate repository.

--- clients/aggregated-client/test.py
+++ clients/aggregated-client/test.py
@@ -4,11 +4,12 @@ from ccpmetrics import CCPMetrics
 
 metrics = CCPMetrics('localhost', 8082)
 
-for i in range(100):
+for i in range(100000):
 	tags = dict()
 	tags["host"] = "host1"
 	tags["service"] = "httpd"
 	metrics.gauge("foo3", random.randint(1, 100), tags)
 	metrics.histogram("foo4", random.randint(1, 100), tags)
 	metrics.increment("foo5", random.randint(1, 100), tags)
-	time.sleep(1)
+	metrics.event("event1", "a wild event occured", "severe", "event1key", "client", "high", tags=tags) 
+	time.sleep(0.01)
